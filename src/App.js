99ef211fa13b9fdf78d3b0bab53a5c2f14af56d5
kazz <kazzzz1001@gmail.com>
2020-12-10 22:02:08 +0900

    update

--- src/App.js
+++ src/App.js
@@ -1,24 +1,54 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import styled from "styled-components";
+
+const Wrapper = styled("div")({
+  width: 600,
+  height: 400,
+  backgroundColor: "black",
+  overflowY: "scroll",
+});
+const HistoryLine = styled("div")({
+  color: "green",
+});
+const CurrentLine = styled("div")({
+  color: "green",
+});
+const Input = styled("input")({
+  fontSize: 16,
+  padding: 0,
+  outline: "none",
+  backgroundColor: "black",
+  color: "green",
+  border: "none",
+});
 
 function App() {
+  const [lines, setLines] = useState([]);
+  const [input, setInput] = useState("");
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Wrapper>
+      {lines.map((l, index) => (
+        <HistoryLine key={index}>user: ~$ {l}</HistoryLine>
+      ))}
+      <CurrentLine>
+        user: ~${" "}
+        <Input
+          type="text"
+          value={input}
+          onChange={(e) => {
+            setInput(e.target.value);
+          }}
+          onKeyDown={(e) => {
+            if (e.key === "Enter") {
+              setLines((l) => [...l, input]);
+              setInput("");
+            } else if (e.key === "Up") {
+              // 上キーでhistory遡る機能(未実装)
+            }
+          }}
+        />
+      </CurrentLine>
+    </Wrapper>
   );
 }
 
